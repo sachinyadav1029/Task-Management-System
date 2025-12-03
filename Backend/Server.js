bf220c4bff0a60ac4c6149a3fcccb680d7d58f24
sachinyadav1029 <121727034+sachinyadav1029@users.noreply.github.com>
2025-12-03 23:06:32 +0530

    Remove node_modules from repo

--- Backend/Server.js
+++ Backend/Server.js
@@ -55,7 +55,6 @@ const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
-
   // Start the task scheduler after server starts
   startTaskScheduler();
 });
